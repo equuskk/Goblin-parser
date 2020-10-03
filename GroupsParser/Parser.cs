a869219b9d3baa58bcc5a2a8b30140dafe4d93b1
Kirill Kuznetsov <kirill.kuzn98@gmail.com>
2020-10-03 12:08:57 +0300

    Добавить папку

--- GroupsParser/Parser.cs
+++ GroupsParser/Parser.cs
@@ -4,7 +4,6 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
-using GroupsParser.Models;
 using HtmlAgilityPack;
 using Newtonsoft.Json;
 using SuperPuperParser.Models;
@@ -22,13 +21,14 @@ namespace GroupsParser
             var sw = new Stopwatch();
             sw.Start();
 
-            var groups = new List<Models.Group>();
+            var groups = new List<Group>();
             var schools = GetSchools();
-            foreach (var school in schools)
+            foreach(var school in schools)
             {
                 Console.WriteLine($"Получение инфы от {school.Name}");
                 groups.AddRange(GetGroups(school.Id));
             }
+
             WriteToFile(groups);
             sw.Stop();
             Console.WriteLine();
@@ -36,23 +36,23 @@ namespace GroupsParser
             Console.WriteLine($"Затрачено времени: {sw.Elapsed.Seconds} секунд {sw.Elapsed.Milliseconds} милисекунд");
         }
 
-        private static List<Models.Group> GetGroups(int schoolId)
+        private static List<Group> GetGroups(int schoolId)
         {
             Console.WriteLine($"Получение списка групп из ВШ {schoolId}");
             var url = $"{EndPoint}/?groups&institution={schoolId}";
             var doc = Web.Load(url);
 
             var groups = doc.DocumentNode
-                .SelectNodes("//a")
-                .Where(x => x.Attributes["href"].Value.StartsWith("?") && int.TryParse(x.ChildNodes[1].InnerText, out _))
-                .Select(x => new Models.Group
-                {
-                    RealId = int.Parse(x.ChildNodes[1].InnerText),
-                    SiteId = int.Parse(x.Attributes["href"].Value.Split('=')[1]),
-                    Name = Regex.Replace(x.ChildNodes[2].InnerText.Trim(), @"\s+", " ")
-                })
-                .Distinct()
-                .ToList();
+                            .SelectNodes("//a")
+                            .Where(x => x.Attributes["href"].Value.StartsWith("?") && int.TryParse(x.ChildNodes[1].InnerText, out _))
+                            .Select(x => new Group
+                            {
+                                RealId = int.Parse(x.ChildNodes[1].InnerText),
+                                SiteId = int.Parse(x.Attributes["href"].Value.Split('=')[1]),
+                                Name = Regex.Replace(x.ChildNodes[2].InnerText.Trim(), @"\s+", " ")
+                            })
+                            .Distinct()
+                            .ToList();
 
             Console.WriteLine($"Получено {groups.Count} групп");
             Console.WriteLine("-------------------------------------------");
@@ -65,25 +65,25 @@ namespace GroupsParser
             var url = EndPoint;
             var doc = Web.Load(url);
             var schools = doc.DocumentNode
-                .SelectNodes("//a")
-                .Where(x => x.Attributes["href"].Value.StartsWith("?"))
-                .Select(x => new School
-                {
-                    Id = int.Parse(x.Attributes["href"].Value.Split('=')[1]),
-                    Url = $"{url}{x.Attributes["href"].Value}",
-                    Name = x.InnerText.Trim()
-                })
-                .GroupBy(x => x.Id)
-                .Select(y => y.First())
-                .ToList();
+                             .SelectNodes("//a")
+                             .Where(x => x.Attributes["href"].Value.StartsWith("?"))
+                             .Select(x => new School
+                             {
+                                 Id = int.Parse(x.Attributes["href"].Value.Split('=')[1]),
+                                 Url = $"{url}{x.Attributes["href"].Value}",
+                                 Name = x.InnerText.Trim()
+                             })
+                             .GroupBy(x => x.Id)
+                             .Select(y => y.First())
+                             .ToList();
 
             return schools;
         }
 
-        private static void WriteToFile(List<Models.Group> groups)
+        private static void WriteToFile(List<Group> groups)
         {
             var str = JsonConvert.SerializeObject(groups);
-            File.WriteAllText("../../../Groups.json", str);
+            File.WriteAllText("../../../Data/Groups.json", str);
         }
     }
 }
